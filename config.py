9c3491cf4b6a435ddfe2d4aa079676563555f005
황석진 <dollking@kookmin.ac.kr>
2021-06-08 06:09:31 +0000

    init

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
 class Config(object):
-    data_name = ''
+    data_name = 'cifar10'
     initial_size = 100
     budge_size = 10
     budge_max = 1000
     max_cycle = int(budge_max / budge_size)
 
-    root_path = ''
-    data_directory = ''
-    summary_directory = ''
-    checkpoint_directory = ''
+    root_path = '/home/D2019063/Active_Learning/'
+    data_directory = 'data'
+    summary_directory = 'board'
+    checkpoint_directory = 'model'
 
     gpu_cnt = 4
 
@@ -18,3 +18,4 @@ class Config(object):
     learning_rate = 0.001
 
     pin_memory = True
+    async_loading = True
