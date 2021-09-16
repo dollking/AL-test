46e7606ff391e0efe7f354d0462bed2b98cdc42f
dollking <dollking@kookmin.ac.kr>
2021-09-16 15:56:22 +0900

    add mAP

--- data/dataset.py
+++ data/dataset.py
@@ -7,7 +7,8 @@ from torchvision.datasets import CIFAR100, CIFAR10
 class Dataset_CIFAR10(CIFAR10):
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
 
-        super(Dataset_CIFAR10, self).__init__(root, transform=transform, target_transform=target_transform)
+        super(Dataset_CIFAR10, self).__init__(root, download=download, train=train, transform=transform,
+                                              target_transform=target_transform)
 
     def __getitem__(self, index):
         img, target = self.data[index], self.targets[index]
@@ -28,7 +29,8 @@ class Dataset_CIFAR10(CIFAR10):
 class Dataset_CIFAR100(CIFAR100):
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
 
-        super(Dataset_CIFAR100, self).__init__(root, transform=transform, target_transform=target_transform)
+        super(Dataset_CIFAR100, self).__init__(root, download=download, train=train, transform=transform,
+                                               target_transform=target_transform)
 
     def __getitem__(self, index):
         img, target = self.data[index], self.targets[index]
