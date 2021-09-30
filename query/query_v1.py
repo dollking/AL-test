409dca83cb5b76d7e88d8f74359d016c0b66edf9
dollking <dollking@kookmin.ac.kr>
2021-09-30 19:29:26 +0900

    add v3

--- query/query_v1.py
+++ query/query_v1.py
@@ -70,11 +70,14 @@ class Query(object):
                 index += 1
         tqdm_batch.close()
 
+        temp_lst = []
         self.test.write(f'step: {step_cnt} -> code count: {len(data_dict.keys())}\n')
         for i in data_dict:
             temp_data = np.array(data_dict[i])[:, 0]
-            self.test.write(f'{temp_data.mean()}/{temp_data.var()}\n')
-        self.test.write('\n')
+            self.test.write(f'{len(temp_data)} - {temp_data.min()}/{temp_data.max()}/{temp_data.mean()}/{temp_data.var()}\n')
+            temp_lst.append(temp_data.mean())
+        temp_lst = np.array(temp_lst)
+        self.test.write(f'{temp_lst.min()}/{temp_lst.max()}/{temp_lst.mean()}\n\n')
 
         sample_set = []
         total_remain = []
