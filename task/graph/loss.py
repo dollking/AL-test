e5648242ba1da054797e2fee88db6482095b7726
dollking <dollking@kookmin.ac.kr>
2021-10-05 16:25:30 +0900

    add v4 (query v3)

--- task/graph/loss.py
+++ task/graph/loss.py
@@ -55,11 +55,11 @@ class GDistanceLoss(nn.Module):
 
     def forward(self, features, loss):
         target1 = torch.sqrt(loss)
+        target1 = target1.detach()
         distance1 = torch.sqrt(torch.sum(torch.pow(features, 2), dim=1))
 
         target2 = torch.sqrt((loss * loss.flip(0))[:loss.size(0) // 2])
         target2 = target2.detach()
-
         distance2 = torch.sqrt(torch.sum(torch.pow((features - features.flip(0))[:features.size(0) // 2], 2), dim=1))
 
         return self.bce(distance1, target1) + self.bce(distance2, target2)
