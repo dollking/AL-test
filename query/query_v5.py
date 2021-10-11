158605089ae86a83d29a24c978dd87efb73bccb6
dollking <dollking@kookmin.ac.kr>
2021-10-11 21:35:13 +0900

    query v4

--- query/query_v5.py
+++ query/query_v5.py
@@ -13,7 +13,7 @@ from data.sampler import Sampler
 
 
 class Query(object):
-    def __init__(self, config, cycle):
+    def __init__(self, config):
         self.config = config
 
         self.initial_size = self.config.initial_size
@@ -36,7 +36,6 @@ class Query(object):
             elif self.config.data_name == 'cifar100':
                 self.dataset = CIFAR100(os.path.join(self.config.root_path, self.config.data_directory),
                                         train=True, download=True, transform=self.train_transform)
-        self.test = open(f'{cycle}.txt', 'w')
 
     def sampling(self, step_cnt, strategy, task):
         if not step_cnt:
