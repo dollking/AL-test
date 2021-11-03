ba334b94d60426d6f2ffcafe5701110b4c21da14
dollking <dollking@kookmin.ac.kr>
2021-11-03 16:55:18 +0900

    add vae

--- query/graph/vae_origin__.py
+++ query/graph/vae_origin__.py
@@ -48,14 +48,25 @@ class Encoder(nn.Module):
                                  out_channels=num_hiddens // 2,
                                  kernel_size=4,
                                  stride=2, padding=1)
+
         self._conv_3 = nn.Conv2d(in_channels=num_hiddens // 2,
                                  out_channels=num_hiddens // 2,
                                  kernel_size=3,
                                  stride=1, padding=1)
+
         self._conv_4 = nn.Conv2d(in_channels=num_hiddens // 2,
                                  out_channels=num_hiddens,
                                  kernel_size=4,
                                  stride=2, padding=1)
+
+        self._residual_stack_1 = ResidualStack(in_channels=num_hiddens // 2,
+                                               num_hiddens=num_hiddens // 2,
+                                               num_residual_layers=num_residual_layers,
+                                               num_residual_hiddens=num_residual_hiddens)
+        self._residual_stack_2 = ResidualStack(in_channels=num_hiddens,
+                                               num_hiddens=num_hiddens,
+                                               num_residual_layers=num_residual_layers,
+                                               num_residual_hiddens=num_residual_hiddens)
 
         self.mu_liner = nn.Linear(4 * 4 * num_hiddens, num_hiddens)
         self.logvar_liner = nn.Linear(4 * 4 * num_hiddens, num_hiddens)
@@ -63,7 +74,8 @@ class Encoder(nn.Module):
         self.batch_norm1 = nn.BatchNorm2d(num_hiddens // 4)
         self.batch_norm2 = nn.BatchNorm2d(num_hiddens // 2)
         self.batch_norm3 = nn.BatchNorm2d(num_hiddens // 2)
-        self.batch_norm4 = nn.BatchNorm2d(num_hiddens)
+        self.batch_norm4 = nn.BatchNorm2d(num_hiddens // 2)
+        self.batch_norm5 = nn.BatchNorm2d(num_hiddens)
 
         self.relu = nn.ReLU(inplace=True)
 
@@ -78,11 +90,16 @@ class Encoder(nn.Module):
 
         x = self._conv_3(x)
         x = self.batch_norm3(x) # 8 x 8
+
+        x = self._residual_stack_1(x)
+        x = self.batch_norm4(x)
         x = self.relu(x)
 
         x = self._conv_4(x)
-        x = self.batch_norm4(x)
+        x = self.batch_norm5(x)
         x = self.relu(x)  # 4 x 4
+
+        x = self._residual_stack_2(x)
 
         x_flat = torch.flatten(x, start_dim=1)
 
@@ -95,14 +112,21 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, in_channels, num_residual_layers, num_residual_hiddens):
         super(Decoder, self).__init__()
-        self.in_channels = in_channels
 
-        self._liner = nn.Linear(in_channels, 4 * 4 * in_channels)
+        self._residual_stack1 = ResidualStack(in_channels=in_channels // 2,
+                                              num_hiddens=in_channels // 2,
+                                              num_residual_layers=num_residual_layers,
+                                              num_residual_hiddens=num_residual_hiddens)
 
-        self._conv_trans1 = nn.ConvTranspose2d(in_channels=in_channels,
+        self._residual_stack2 = ResidualStack(in_channels=in_channels // 8,
+                                              num_hiddens=in_channels // 8,
+                                              num_residual_layers=num_residual_layers,
+                                              num_residual_hiddens=num_residual_hiddens)
+
+        self._conv_trans_1 = nn.ConvTranspose2d(in_channels=in_channels,
                                                 out_channels=in_channels // 2,
                                                 kernel_size=4,
-                                                stride=2, padding=1)
+                                                stride=4)
 
         self._conv_trans_2 = nn.ConvTranspose2d(in_channels=in_channels // 2,
                                                 out_channels=in_channels // 4,
@@ -110,27 +134,43 @@ class Decoder(nn.Module):
                                                 stride=2, padding=1)
 
         self._conv_trans_3 = nn.ConvTranspose2d(in_channels=in_channels // 4,
+                                                out_channels=in_channels // 8,
+                                                kernel_size=4,
+                                                stride=2, padding=1)
+
+        self._conv_trans_4 = nn.ConvTranspose2d(in_channels=in_channels // 8,
                                                 out_channels=3,
                                                 kernel_size=4,
                                                 stride=2, padding=1)
 
         self.batch_norm1 = nn.BatchNorm2d(in_channels // 2)
-        self.batch_norm2 = nn.BatchNorm2d(in_channels // 4)
+        self.batch_norm2 = nn.BatchNorm2d(in_channels // 2)
+        self.batch_norm3 = nn.BatchNorm2d(in_channels // 4)
+        self.batch_norm4 = nn.BatchNorm2d(in_channels // 8)
+        self.batch_norm5 = nn.BatchNorm2d(in_channels // 8)
 
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, inputs):
-        x = self._liner(inputs).view([-1, self.in_channels, 4, 4])
-
-        x = self._conv_trans_1(x)
+        x = self._conv_trans_1(inputs)
         x = self.batch_norm1(x)
-        x = self.relu(x)
 
-        x = self._conv_trans_2(x)
+        x = self._residual_stack1(x)
         x = self.batch_norm2(x)
         x = self.relu(x)
 
-        return self._conv_trans_3(x)
+        x = self._conv_trans_2(x)
+        x = self.batch_norm3(x)
+        x = self.relu(x)
+
+        x = self._conv_trans_3(x)
+        x = self.batch_norm4(x)
+
+        x = self._residual_stack2(x)
+        x = self.batch_norm5(x)
+        x = self.relu(x)
+
+        return self._conv_trans_4(x)
 
 
 class AE(nn.Module):
